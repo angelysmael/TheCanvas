7bc4ed71d715c59a0f6d3c2791f0a67ee01727ce
James Del Pilar <71074350+jamesdelpilar@users.noreply.github.com>
2026-04-08 21:50:37 -0400

    Added a test Image and Form component

--- app/ContactForm/page.tsx
+++ app/ContactForm/page.tsx
@@ -1,10 +1,23 @@
+import Form from 'next/form'
+
 export default function ContactForm() {
   return (
     <div>
       <h1>Contact Form</h1>
       <p>
-        This is a CONTACT FORM. ADD Something INTERACTIVE
+        This is a CONTACT FORM. 
       </p>
+
+      <br></br>
+      <p>
+        Form component sample:
+      </p>
+
+      <Form action="/search" className="flex items-center gap-2 max-w-sm">
+      <input name="query" />
+      <button type="submit" className="rounded-lg border border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">Submit</button>
+      </Form>
+      
     </div>
   );
 }
--- app/Main/page.tsx
+++ app/Main/page.tsx
@@ -1,10 +1,22 @@
+import Image from 'next/image'
+
 export default function Main() {
   return (
     <div>
       <h1>MAIN</h1>
       <p>
-        This is the MAIN PAGE. ADD Something INTERACTIVE
+        This is the MAIN PAGE. 
       </p>
+      <br></br>
+
+      <p>Test image component: </p>
+      <Image
+      src="/next.svg"
+      width={250}
+      height={250}
+      alt="Test Image"
+      />
+
     </div>
   );
 }
